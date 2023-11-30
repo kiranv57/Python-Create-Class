086c6f3f50455aaface7ea5a75716f9bd8ca4b7d
kiranv57 <151369756+kiranv57@users.noreply.github.com>
2023-11-30 03:20:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    # directly creating class properties
+    # these are class level attributes and shared by all     instances
+    height = 3
+    width = 2
+   # using constructor to create instance properties
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+  
